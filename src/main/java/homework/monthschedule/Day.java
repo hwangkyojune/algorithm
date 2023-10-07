0e7e6232e854ddc4d2c618937ba9ed4f7cb96462
hwangkyojune <shane5969@naver.com>
2023-10-08 01:54:33 +0900

    MonthSchedule

---
+++ src/main/java/homework/monthschedule/Day.java
@@ -0,0 +1,19 @@
+package homework.monthschedule;
+
+public class Day {
+    private String work;
+    public void set(String work){
+        this.work = work;
+    }
+    public String get(){
+        return work;
+    }
+    public void show(){
+        if(work==null){
+            System.out.println("할 일이 없습니다.");
+        }
+        else{
+            System.out.println(work+"입니다.");
+        }
+    }
+}
---
+++ src/main/java/homework/monthschedule/MonthSchedule.java
@@ -0,0 +1,13 @@
+package homework.monthschedule;
+
+public class MonthSchedule {
+    public Day[] month;
+    public MonthSchedule(int day){
+        month = new Day[day];
+    }
+    public void input(int i,String work){
+    }
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
