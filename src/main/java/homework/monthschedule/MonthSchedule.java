048f6d5094137d74eb9ca8135a8af091d25706ee
hwangkyojune <shane5969@naver.com>
2023-10-09 00:53:55 +0900

    implement MonthSchedule

--- src/main/java/homework/monthschedule/MonthSchedule.java
+++ src/main/java/homework/monthschedule/MonthSchedule.java
@@ -1,11 +1,24 @@
 package homework.monthschedule;
+import homework.Circle;
 
 public class MonthSchedule {
     public Day[] month;
-    public MonthSchedule(int day){
-        month = new Day[day];
+    public String a;
+    public MonthSchedule(int day, String a, Day[] month){
+        this.month = new Day[day];
     }
-    public void input(int i,String work){
+
+    //날짜에 할일 입력
+    public void input(int day,String work){
+        month[day].set(work);
+    }
+    //특정 날짜의 할일 반환
+    public String view(int day){
+        return month[day].get();
+    }
+    //한달의 모든 일정 반환
+    public Day[] viewMonth(){
+        return month;
     }
     public static void main(String[] args) {
         System.out.println();
